55254f4395cfacac9a23b2d6131097a88c880b5a
Me <me@me.com>
2016-11-27 10:08:54 +0530

    my new test

---
+++ phpinfo.php
@@ -0,0 +1,14 @@
+<?php
+$data = array('a'=>'aaaa','b'=>'bbbb');
+echo $json_string = json_encode($data, JSON_PRETTY_PRINT);
+//phpinfo();
+
+ ?><select name="field">
+    <optgroup label="Group 1"  value="Value 1">
+        <option value="Value 1">Label 1</option>
+        <option value="Value 2">Label 2</option>
+    </optgroup>
+    <optgroup label="Group 2"  value="Value 1">
+        <option value="Value 3">Label 3</option>
+    </optgroup>
+</select>
